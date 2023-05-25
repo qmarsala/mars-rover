23b0d8b91826371d409bdea11273002fd67752ad
Quinten Marsala <quinten.marsala@gmail.com>
2023-05-25 14:14:47 -0700

    move north

---
+++ src/MarsRover/MarsRoverCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿namespace MarsRover;
+public record Position(int X, int Y);
+public record RoverCommandSequence(params RoverCommand[] Commands);
+public record RoverPosition(CardinalDirection Heading, Position Position);
+public record Plateau(Position BottomLeft, Position TopRight);
+
+public enum RoverCommand
+{
+    None,
+    MoveForward,
+    RotateRight,
+    RotateLeft
+}
+
+public enum CardinalDirection
+{
+    North, East, South, West
+}
+
+public interface ICommandRover
+{
+    RoverPosition Execute(RoverPosition startingPosition, RoverCommandSequence commandSequence);
+}
+
+public class MarsRoverCommandHandler : ICommandRover
+{
+    public RoverPosition Execute(RoverPosition startingPosition, RoverCommandSequence commandSequence)
+    {
+        var position = new RoverPosition(startingPosition.Heading, startingPosition.Position);
+        foreach (var command in commandSequence.Commands)
+        {
+            position = HandleCommand(position, command);
+        }
+        return position;
+    }
+
+    private RoverPosition HandleCommand(RoverPosition startingPosition, RoverCommand command)
+    {
+        if (command is RoverCommand.MoveForward)
+        {
+            return new RoverPosition(CardinalDirection.North, new(0, 1));
+        }
+        else
+        {
+            return new RoverPosition(CardinalDirection.North, new(0, 0));
+        }
+    }
+}
