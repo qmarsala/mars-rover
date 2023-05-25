b13176afcb317d4a9a9e45a08689ae751a0cec1b
Quinten Marsala <quinten.marsala@gmail.com>
2023-05-25 16:57:07 -0700

    test boundary parser

---
+++ src/MarsRover.Tests/BoundaryParserTests.cs
@@ -0,0 +1,16 @@
+﻿namespace MarsRover.Tests;
+
+public class BoundaryParserTests
+{
+    [Fact]
+    public void Should_parse_boundary()
+    {
+        var inputString = "5 5";
+        var expectedBoundary = new Boundary(new(0, 0), new(5, 5));
+
+        IParseInput<Boundary> boundaryParser = new BoundaryParser();
+        var result = boundaryParser.Parse(inputString);
+
+        result.ShouldBe(expectedBoundary);
+    }
+}
