781eca7cfcf810c8a45fb7fcdfa24eb8036aa049
Quinten Marsala <quinten.marsala@gmail.com>
2023-05-25 17:30:03 -0700

    test bad position data

--- src/MarsRover.Tests/RoverPositionParserTests.cs
+++ src/MarsRover.Tests/RoverPositionParserTests.cs
@@ -13,4 +13,30 @@ public class RoverPositionParserTests
 
         result.ShouldBe(expectedRoverPosition);
     }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData(" ")]
+    [InlineData("     ")]
+    public void Should_parse_badinput(string inputString)
+    {
+        var expectedRoverPosition = new RoverPosition(CardinalDirection.None, new Position(0, 0));
+
+        IParseInput<RoverPosition> roverPosParser = new RoverPositionParser();
+        var result = roverPosParser.Parse(inputString);
+
+        result.ShouldBe(expectedRoverPosition);
+    }
+
+    [Fact]
+    public void Should_parse_roverpositions_outof_badinput()
+    {
+        var inputString = "  2 3  S ";
+        var expectedRoverPosition = new RoverPosition(CardinalDirection.South, new Position(2, 3));
+
+        IParseInput<RoverPosition> roverPosParser = new RoverPositionParser();
+        var result = roverPosParser.Parse(inputString);
+
+        result.ShouldBe(expectedRoverPosition);
+    }
 }
