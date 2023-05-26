24ca05f7f43286d0f075f39433b8632de22d582a
Quinten Marsala <quinten.marsala@gmail.com>
2023-05-25 17:12:33 -0700

    test rover pos parser

---
+++ src/MarsRover.Tests/RoverPositionParserTests.cs
@@ -0,0 +1,16 @@
+﻿namespace MarsRover.Tests;
+
+public class RoverPositionParserTests
+{
+    [Fact]
+    public void Should_parse_rover_positions()
+    {
+        var inputString = "1 2 S";
+        var expectedRoverPosition = new RoverPosition(CardinalDirection.South, new Position(1, 2));
+
+        IParseInput<RoverPosition> roverPosParser = new RoverPositionParser();
+        var result = roverPosParser.Parse(inputString);
+
+        result.ShouldBe(expectedRoverPosition);
+    }
+}
